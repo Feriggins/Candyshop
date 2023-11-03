c905be1f8e8491382b916ce97c3d4842e5d70962
Feriggins <141282016+Feriggins@users.noreply.github.com>
2023-11-03 19:57:38 -0400

    candyshop update

--- controllers/places.js
+++ controllers/places.js
@@ -33,6 +33,25 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+res.render('places/show', { place: places[id] })
+
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show')
+  }
+})
+
+
+
+
 router.post('/new',(req, res) => {
 
   const name = req?.body?.name || 'Default Name'
