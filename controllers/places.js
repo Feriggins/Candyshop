b1a3abc4f42098f11a6e05c1603341023fa7673c
Feriggins <141282016+Feriggins@users.noreply.github.com>
2023-11-13 20:06:48 -0500

    finished 7

--- controllers/places.js
+++ controllers/places.js
@@ -48,7 +48,7 @@ router.get('/:id', (req, res) => {
 
 router.get('/:id/edit', (req, res) => {
   let id= Number(req.params.id)
-  if(isNaN(ID)) {
+  if(isNaN(id)) {
     res.render('error404')
   }
   else if (!places[id]) {
@@ -56,7 +56,7 @@ router.get('/:id/edit', (req, res) => {
 
 }
 else {
-  res.render('places/edit', {place: places[id]})
+  res.render('places/edit', {place: places[id], id})
 }
 })
 
@@ -90,7 +90,34 @@ router.delete('/:id', (req, res) => {
   }
   else {
     places.splice(id, 1)
-    res.redirect('/places')
+    res.redirect(`/places/${id}`)
+  }
+})
+router.put('/:id', (req, res) => {
+  console.log(req.body);
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
   }
 })
 
--- views/Places/edit.jsx
+++ views/Places/edit.jsx
@@ -1,30 +1,54 @@
 const React = require('react')
 const Def = require('../default.jsx')
 
-function edit_form (data) {
-    return (
-      <Def>
-        <main>
-    <form method="POST" action={`/places/${data.place.id}?_method=PUT`}/>
-  ...   
-        <div className="form-group col-sm-6">
-                <label htmlFor="name">Place Name</label>
-                <input id="name"   />
-        <div/>
-        <div className="form-group col-sm-6">
-        <label htmlFor="name">Place picture</label>
-                <input id="pic"   
-
-                    className="form-control"
-                    id="name"
-                    value={data.place.name}
-                    required/>
-    </form>
-    </div>
-    </main>
+function edit_form(data) {
+  return (
+    <Def>
+      <main>
+        <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+          <div class="container">
+            <div class="row">
+              <div class="col text-center">
+                <h1>Edit Place</h1>
+              </div>
+            </div>
+            <div class="row mt-5">
+              <div class="col-6">
+                  <label for="name" class="form-label">Place Name</label>
+                  <input type="text" class="form-control" id="name" name="name" defaultValue={`${data.place.name}`}/>
+              </div>
+              <div class="col-6">
+                  <label for="pic" class="form-label">Place Picture</label>
+                  <input type="text" class="form-control" id="pic" name="pic" defaultValue={`${data.place.pic}`}/>
+              </div>
+            </div>
+            <div class="row mt-5">
+              <div class="col-6">
+                  <label for="city" class="form-label">Place City</label>
+                  <input type="text" class="form-control" id="city" name="city" defaultValue={`${data.place.city}`}/>
+              </div>
+              <div class="col-6">
+                  <label for="state" class="form-label">Place State</label>
+                  <input type="text" class="form-control" id="state" name="state" defaultValue={`${data.place.state}`}/>
+              </div>
+            </div>
+            <div class="row mt-5">
+              <div class="col">
+                  <label for="cuisines" class="form-label">Place Cuisine</label>
+                  <input type="text" class="form-control" id="cuisines" name="cuisines" defaultValue={`${data.place.cuisines}`}/>
+              </div>
+            </div>
+            <div class="row mt-5">
+              <div class="col-2">
+                <button type="submit" class="btn btn-primary mb-3">Edit Place</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </main>
 
     </Def>
-    )
+  )
 }
 
 module.exports = edit_form
